246efbec395bb75561b470fde821a440ee9a0486
Gabriel <gaplogan@gmail.com>
2020-12-25 14:48:32 -0300

    2020.12.25.14.48

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace cs_1._12_AvisoFesta5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vezes;
+            string Frase;
+
+            Console.Clear();
+
+            Console.Write("Digite a frase: ");
+            Frase = Console.ReadLine();
+
+            vezes = 0;
+
+            do
+            {
+                Console.Write("Digite o número de repetições: ");
+                vezes = int.Parse(Console.ReadLine());
+            } while (vezes < 1);
+
+            Console.WriteLine();
+
+            for (int i = 0; i < vezes; i++)
+            {
+                Console.WriteLine(Frase);
+            }
+        }
+    }
+}
